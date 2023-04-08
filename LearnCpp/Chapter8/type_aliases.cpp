7c6daaf26a478c27ab96d533dd85da069fca87f8
David Gugea <davidgugea@yahoo.com>
2023-04-08 21:43:41 +0200

    Chapter 8

---
+++ LearnCpp/Chapter8/type_aliases.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <utility>
+
+using VectPairSI = std::vector<std::pair<std::stirng, int>>; // make VectPairSI an alias for this type
+
+bool hasDuplicates(VectPairSI pairlist) {
+	/* code */
+	return false;
+}
+
+int main()
+{
+	VectPairSI pairlist;
+
+	return 0;
+}
